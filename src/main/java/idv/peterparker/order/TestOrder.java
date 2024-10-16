0c9455a5ec4cb24feff75c0aa9f6bbeef80b7901
Scott Chang <scggyy1226@gmail.com>
2024-10-16 15:17:58 +0800

    Scott test commit

---
+++ src/main/java/idv/peterparker/order/TestOrder.java
@@ -0,0 +1,13 @@
+package idv.peterparker.order;
+
+public class TestOrder {
+
+	private int orderId;
+	
+	public static void main(String[] args) {
+		int test01 = 10;
+		System.out.println("test Order" + test01);
+
+	}
+
+}
